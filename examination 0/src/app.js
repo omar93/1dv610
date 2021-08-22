d698abba1bb18366684cff5e85bf57cfcd37a898
Omar Alhamad <omar93@hotmail.se>
2021-08-22 03:34:03 +0200

    exam 0 is done

--- examination 0/src/app.js
+++ examination 0/src/app.js
@@ -1,7 +1,27 @@
+let container = document.querySelector('#container')
 let form = document.querySelector('#name-form')
+let firstName = document.querySelector('#name')
+let welcomeText = document.querySelector('#welcome-text')
+let face = document.querySelector('#face')
 
 form.addEventListener('submit', handleNameSubmit)
 
 function handleNameSubmit(e) {
-  e.preventDefault()
+ e.preventDefault()
+ form.parentElement.removeChild(form)
+ document.querySelector('body').classList.add('oj')
+
+ welcomeText.classList.add('fade-in')
+ welcomeText.classList.remove('hidden')
+
+ face.classList.add('fade-in')
+ face.classList.remove('hidden')
+
+ setTimeout( _ => {
+  let nameText = document.createElement('p')
+  nameText.textContent = firstName.value
+  nameText.classList.add('fade-in')
+  container.appendChild(document.createElement('br'))
+  container.appendChild(nameText)
+  }, 2000)
 }
